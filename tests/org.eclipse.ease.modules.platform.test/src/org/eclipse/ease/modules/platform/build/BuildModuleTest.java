e1745891a3a525c00038c8bd6dadda3a5d5b016a
Martin SCHREIBER <m.schreiber@bachmann.info>
2018-01-25 07:13:19 +0100

    Bug 529357: Expose the build kind constant values
    Adding a new "Build"-Module which exposes the build kind constants and a
    method "build" which triggers a build on a project.
    
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=529357
    Change-Id: I887fb25b0fc6ad9ba40e278d7fa855796dc2633e
    Signed-off-by: Martin SCHREIBER <m.schreiber@bachmann.info>

---
+++ tests/org.eclipse.ease.modules.platform.test/src/org/eclipse/ease/modules/platform/build/BuildModuleTest.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Bachmann electronic GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bachmann electronic GmbH - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.ease.modules.platform.build;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test Class for the {@link BuildModule}
+ */
+public class BuildModuleTest {
+
+	private BuildModule fModule;
+	private IWorkspaceRoot fRoot;
+	private IProject fProject;
+
+	@Before
+	public void setup() {
+		fProject = mock(IProject.class);
+		fRoot = mock(IWorkspaceRoot.class);
+		fModule = new BuildModule(fRoot);
+		when(fRoot.getProject("MyProject")).thenReturn(fProject);
+	}
+
+	@Test
+	public void buildDoesCallProjectsBuildMethodIfProjectExists() throws CoreException {
+		when(fProject.isAccessible()).thenReturn(true);
+		fModule.build("MyProject", BuildModule.CLEAN_BUILD);
+		verify(fProject, times(1)).build(eq(IncrementalProjectBuilder.CLEAN_BUILD), any(NullProgressMonitor.class));
+	}
+
+	@Test
+	public void buildDoesNotCallProjectsBuildMethodIfProjectNotExists() throws CoreException {
+		when(fProject.isAccessible()).thenReturn(false);
+		fModule.build("MyProject", BuildModule.CLEAN_BUILD);
+		verify(fProject, never()).build(eq(IncrementalProjectBuilder.CLEAN_BUILD), any(NullProgressMonitor.class));
+	}
+
+}
