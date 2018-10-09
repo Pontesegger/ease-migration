5e6dc8045092bfce424ca0bf9461b80126e43222
Martin Kloesch <martin@kmh-solutions.com>
2018-10-09 16:08:13 +0200

    Bug 539962 - Python imp module deprecated
    Ignore deprecation warning (for now)
    
    Change-Id: I8ff87267ed4ab4339447deb8b217436a406c0237
    Signed-off-by: Martin Kloesch <martin@kmh-solutions.com>

--- plugins/org.eclipse.ease.lang.python/pysrc/pep302.py
+++ plugins/org.eclipse.ease.lang.python/pysrc/pep302.py
@@ -1,5 +1,20 @@
+'''
+Copyright (c) 2018 Christian Pontesegger
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/epl-v10.html
+
+Contributors:
+ * Christian Pontesegger - initial API and implementation
+'''
 import sys
-import imp
+
+# Fixme: Check version compatibility and refactor
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    import imp
  
 class EASEImporter(object):
     """EASE module loader.
