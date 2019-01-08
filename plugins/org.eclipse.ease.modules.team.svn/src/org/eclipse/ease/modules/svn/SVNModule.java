84808d658df4750d947508870b79305e82f33464
Christian Pontesegger <christian.pontesegger@web.de>
2019-01-08 10:13:57 +0100

    Bug 543236: [SVN] SVN operations do not work in headless mode
      added headless operation
      cleaned up API to only have one import method
    
    Change-Id: I45be7e6cf48b934e30c3a7e4e536e8320465ab85

--- plugins/org.eclipse.ease.modules.team.svn/src/org/eclipse/ease/modules/svn/SVNModule.java
+++ plugins/org.eclipse.ease.modules.team.svn/src/org/eclipse/ease/modules/svn/SVNModule.java
@@ -14,9 +14,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.ease.modules.AbstractScriptModule;
 import org.eclipse.ease.modules.ScriptParameter;
 import org.eclipse.ease.modules.WrapToScript;
+import org.eclipse.ease.modules.platform.UIModule;
 import org.eclipse.ease.tools.ResourceTools;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -34,33 +36,28 @@ import org.eclipse.team.svn.ui.extension.ExtensionsManager;
 import org.eclipse.team.svn.ui.utility.UIMonitorUtility;
 
 /**
- * Provides functions to access and operate on SVN repositories through.
+ * Provides functions to access and operate on SVN repositories.
  */
 public class SVNModule extends AbstractScriptModule {
 
 	/**
-	 * Creates repository location.
-	 *
-	 * Creates repository location with <i>rootUrl</i>, if location already exists: return existing location
+	 * Creates a new repository location. If the location already exists the existing location gets returned.
 	 *
 	 * @param rootUrl
 	 *            defines the root URL of the repository
 	 * @param username
-	 *            registered username of the repository
+	 *            username to be used
 	 * @param password
-	 *            password depending to the username
-	 * @return repository location which is created, or the existing repository location
+	 *            password for authentication
+	 * @return repository location
 	 */
 	@WrapToScript
 	public IRepositoryLocation createRepositoryLocation(final String rootUrl, @ScriptParameter(defaultValue = ScriptParameter.NULL) final String username,
 			@ScriptParameter(defaultValue = ScriptParameter.NULL) final String password) {
 
-		final IRepositoryLocation[] locations = SVNRemoteStorage.instance().getRepositoryLocations();
-
-		for (final IRepositoryLocation location : locations) {
-			if ((location.getUrlAsIs().equals(rootUrl)) || (location.getUrl().equals(rootUrl))) {
+		for (final IRepositoryLocation location : SVNRemoteStorage.instance().getRepositoryLocations()) {
+			if ((location.getUrlAsIs().equals(rootUrl)) || (location.getUrl().equals(rootUrl)))
 				return location;
-			}
 		}
 
 		final IRepositoryLocation location = SVNRemoteStorage.instance().newRepositoryLocation();
@@ -73,17 +70,18 @@ public class SVNModule extends AbstractScriptModule {
 
 		if (username != null)
 			location.setUsername(username);
+
 		if (password != null)
 			location.setPassword(password);
 
 		location.setPasswordSaved(true);
 
 		final AddRepositoryLocationOperation operation = new AddRepositoryLocationOperation(location);
-		final CompositeOperation op = new CompositeOperation(operation.getId(), operation.getMessagesClass());
-		op.add(operation);
-		op.add(new SaveRepositoryLocationsOperation());
+		final CompositeOperation compositeOperation = new CompositeOperation(operation.getId(), operation.getMessagesClass());
+		compositeOperation.add(operation);
+		compositeOperation.add(new SaveRepositoryLocationsOperation());
 
-		Display.getDefault().syncExec(() -> UIMonitorUtility.doTaskNowDefault(op, false));
+		executeOperation(compositeOperation);
 
 		return location;
 	}
@@ -94,60 +92,43 @@ public class SVNModule extends AbstractScriptModule {
 	 * @param rootLocation
 	 *            can be a string (to generate RepositoryLocation automatically) or already a RepositoryLocation
 	 * @param projectLocations
-	 *            array from relative paths to project locations
+	 *            array from relative paths to project locations within the repository. When not provided all projects from trunk will be imported (not
+	 *            recursive)
 	 */
 	@WrapToScript
-	public void importProjectFromSVN(Object rootLocation, final String[] projectLocations) {
-		if (rootLocation instanceof IRepositoryResource) {
-		} else {
+	public void importProjectsFromSVN(Object rootLocation, @ScriptParameter(defaultValue = ScriptParameter.NULL) final String[] projectLocations) {
+		if (!(rootLocation instanceof IRepositoryResource))
 			rootLocation = createRepositoryLocation(rootLocation.toString(), null, null);
-		}
 
-		final List<IRepositoryResource> doCeckout_tmp = new ArrayList<>();
-		for (final String location : projectLocations) {
-			final IRepositoryResource projectResource = SVNRemoteStorage.instance().asRepositoryResource((IRepositoryLocation) rootLocation,
-					((IRepositoryLocation) rootLocation).getUrl() + "/" + location, false);
-			doCeckout_tmp.add(projectResource);
-		}
+		IRepositoryResource[] checkoutResources;
+		if (projectLocations != null) {
+			// checkout user defined projects
+			final List<IRepositoryResource> checkoutList = new ArrayList<>();
 
-		final IRepositoryResource[] doCeckout = doCeckout_tmp.toArray(new IRepositoryResource[] {});
+			for (final String location : projectLocations) {
+				final IRepositoryResource projectResource = SVNRemoteStorage.instance().asRepositoryResource((IRepositoryLocation) rootLocation,
+						((IRepositoryLocation) rootLocation).getUrl() + "/" + location, false);
+				checkoutList.add(projectResource);
+			}
+			checkoutResources = checkoutList.toArray(new IRepositoryResource[checkoutList.size()]);
 
-		Display.getDefault().syncExec(() -> {
-			final Shell sh = Display.getDefault().getActiveShell();
-			final IActionOperation op = ExtensionsManager.getInstance().getCurrentCheckoutFactory().getCheckoutOperation(sh, doCeckout, null, true, null,
-					SVNDepth.INFINITY, false);
-			UIMonitorUtility.doTaskNowDefault(op, true);
-		});
-	}
-
-	/**
-	 * Import all projects from a repository location. It scans the location and all projects found are imported.
-	 *
-	 * @param rootLocation
-	 *            can be a string (to generate RepositoryLocation automatically) or already a RepositoryLocation
-	 */
-	@WrapToScript
-	public void importProjectsFromSVN(Object rootLocation) {
-		IRepositoryResource repositoryResource = null;
-		if (rootLocation instanceof IRepositoryResource) {
-			repositoryResource = (IRepositoryResource) rootLocation;
 		} else {
-			final IRepositoryLocation repositoryLocation = createRepositoryLocation(rootLocation.toString(), null, null);
-			repositoryResource = SVNRemoteStorage.instance().asRepositoryResource(repositoryLocation, repositoryLocation.getUrl(), false);
-		}
-		final IRepositoryResource res = repositoryResource;
-		final IRepositoryResource[] doCeckout = new IRepositoryResource[] { res };
-		final LocateProjectsOperation locateProjectsOp = new LocateProjectsOperation(doCeckout,
-				ExtensionsManager.getInstance().getCurrentCheckoutFactory().getLocateFilter(), 5);
-		Display.getDefault().syncExec(() -> {
-			final Shell sh = Display.getDefault().getActiveShell();
-			// Execute the locate project operation first, to get a list of all repository projects
-			UIMonitorUtility.doTaskNowDefault(locateProjectsOp, true);
-			final IActionOperation op = ExtensionsManager.getInstance().getCurrentCheckoutFactory().getCheckoutOperation(sh,
-					locateProjectsOp.getRepositoryResources(), null, true, null, SVNDepth.INFINITY, false);
-			UIMonitorUtility.doTaskNowDefault(op, true);
-		});
+			// checkout all projects
+			final IRepositoryResource res = (IRepositoryResource) rootLocation;
+			final IRepositoryResource[] doCeckout = new IRepositoryResource[] { res };
+			final LocateProjectsOperation locateProjectsOp = new LocateProjectsOperation(doCeckout,
+					ExtensionsManager.getInstance().getCurrentCheckoutFactory().getLocateFilter(), 5);
 
+			// Execute the locate project operation first, to get a list of all repository projects
+			executeOperation(locateProjectsOp);
+			checkoutResources = locateProjectsOp.getRepositoryResources();
+		}
+
+		final Shell shell = UIModule.isHeadless() ? null : Display.getDefault().getActiveShell();
+		final IActionOperation op = ExtensionsManager.getInstance().getCurrentCheckoutFactory().getCheckoutOperation(shell, checkoutResources, null, true, null,
+				SVNDepth.INFINITY, false);
+
+		executeOperation(op);
 	}
 
 	/**
@@ -166,5 +147,18 @@ public class SVNModule extends AbstractScriptModule {
 		}
 
 		return -1;
+	}
+
+	/**
+	 * Execute an internal SVN operation. Uses UI if available.
+	 *
+	 * @param operation
+	 *            operation to execute
+	 */
+	private void executeOperation(IActionOperation operation) {
+		if (UIModule.isHeadless())
+			operation.run(new NullProgressMonitor());
+		else
+			Display.getDefault().syncExec(() -> UIMonitorUtility.doTaskNowDefault(operation, false));
 	}
 }
