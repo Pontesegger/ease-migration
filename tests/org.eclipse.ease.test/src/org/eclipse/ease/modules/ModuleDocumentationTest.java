e8137d6ab09bb7675dc6e197f64e980ce4ffe44a
Christian Pontesegger <christian.pontesegger@web.de>
2021-05-07 08:40:42 +0200

    Bug 573422: Module documentation not available on integration builds
      maven-javadoc-plugin parameter name changed. kept old settings for
      reference, added new settings, too.
    
    Change-Id: I869d7a25e454a882213e54af6cee943c738249cd

---
+++ tests/org.eclipse.ease.test/src/org/eclipse/ease/modules/ModuleDocumentationTest.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Christian Pontesegger and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Christian Pontesegger - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.ease.modules;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.eclipse.core.runtime.Platform;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ModuleDocumentationTest {
+
+	@Test
+	@DisplayName("module documentation exists")
+	public void module_documentation_exists() {
+		assertNotNull(Platform.getBundle("org.eclipse.ease").getEntry("/help/module_org.eclipse.ease.environment.html"));
+	}
+}
